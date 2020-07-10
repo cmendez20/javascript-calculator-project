00a2fa31aaff2941736dce8eebb43d67c411fe83
Chris Mendez <cmendez20@utexas.edu>
2020-07-09 21:40:59 -0500

    finished v1 of calculator app

--- scripts.js
+++ scripts.js
@@ -1,29 +1,83 @@
 let resultScreen = document.querySelector('.result-screen');
-let numArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let operatorArray = ['&divide;', 'x', '-', '+'];
+let numOne = undefined;
+let numTwo = undefined;
+let chosenOperator = undefined;
+
+const clearBtn = document.querySelector('#clearBtn');
+const deleteBtn = document.querySelector('#deleteBtn');
+const subtractBtn = document.querySelector('#subtractBtn');
+const multiplyBtn = document.querySelector('#multiplyBtn');
+const addBtn = document.querySelector('#addBtn');
+const divideBtn = document.querySelector('#divideBtn');
+const equalBtn = document.querySelector('#equalBtn');
 
 let btnNums = document.querySelectorAll('.num');
 console.log(btnNums);
 
 btnNums.forEach(item => {
   item.addEventListener('click', e => {
-    if (numArray.includes(e.target.innerHTML)) {
-    resultScreen.innerHTML = e.target.innerHTML;
-    } else if (operatorArray.includes(e.target.innerHTML)) {
-      console.log('this is an operator');
-    }
+    if (resultScreen.textContent === '0') {
+      resultScreen.textContent = '';
+    } 
+    updateResultScreen(e.target.textContent);
   });
 });
 
 
+clearBtn.addEventListener('click', () => {
+  resultScreen.textContent = '0';
+});
 
-for (i = 0; i < btnNums.length; i++) {
-  let numPad = parseInt(btnNums[i].textContent);
-  console.log(numPad, typeof numPad);
-}
+deleteBtn.addEventListener('click', () => {
+  if (resultScreen.textContent === '0' || resultScreen.textContent.length < 1) {
+    resultScreen.textContent = '0';
+  } else if (resultScreen.textContent.length < 2) {
+    resultScreen.textContent = '0';
+  } else {
+    let originalScreen = resultScreen.textContent;
+    let newScreen = originalScreen.slice(0, -1);
+    resultScreen.textContent = newScreen;
+  }
+});
 
+addBtn.addEventListener('click', () => {
+  numOne = resultScreen.textContent;
+  chosenOperator = '+'
+  resultScreen.textContent = '0';
+});
 
-// function changeResultScreen() {
-//   resultScreen = 
-// } 
+subtractBtn.addEventListener('click', () => {
+  numOne = resultScreen.textContent;
+  chosenOperator = '-'
+  resultScreen.textContent = '0';
+});
 
+multiplyBtn.addEventListener('click', () => {
+  numOne = resultScreen.textContent;
+  chosenOperator = 'x'
+  resultScreen.textContent = '0';
+});
+
+divideBtn.addEventListener('click', () => {
+  numOne = resultScreen.textContent;
+  chosenOperator = '&divide;'
+  resultScreen.textContent = '0';
+});
+
+equalBtn.addEventListener('click', () => {
+  numTwo = resultScreen.textContent;
+  if (chosenOperator === '-') {
+    resultScreen.textContent = numOne - numTwo;
+  } else if (chosenOperator === '+') {
+    resultScreen.textContent = parseInt(numOne) + parseInt(numTwo);
+  } else if (chosenOperator === 'x') {
+    resultScreen.textContent = numOne * numTwo;
+  } else {
+    resultScreen.textContent = numOne / numTwo;
+  }
+  // numTwo = undefined;
+});
+
+function updateResultScreen(userInput) {
+  resultScreen.textContent += userInput; 
+} 
